de7a3fe72925009b9aa4b645df561af3fbb8e7ec
siv <sivsivsree@gmail.com>
2019-04-14 07:43:13 +0400

    removed fyyn

--- lib/gui/helloGui.go
+++ lib/gui/helloGui.go
@@ -1,21 +1,21 @@
 package gui
 
-import (
-	"fyne.io/fyne/app"
-	"fyne.io/fyne/widget"
-)
-
-// ScreenOne is
-func ScreenOne() {
-	application := app.New()
-
-	w := application.NewWindow("Hello")
-	w.SetContent(widget.NewVBox(
-		widget.NewLabel("Hello Siv..!"),
-		widget.NewButton("Quit", func() {
-			application.Quit()
-		}),
-	))
-
-	w.ShowAndRun()
-}
+//import (
+//	"fyne.io/fyne/app"
+//	"fyne.io/fyne/widget"
+//)
+//
+//// ScreenOne is
+//func ScreenOne() {
+//	application := app.New()
+//
+//	w := application.NewWindow("Hello")
+//	w.SetContent(widget.NewVBox(
+//		widget.NewLabel("Hello Siv..!"),
+//		widget.NewButton("Quit", func() {
+//			application.Quit()
+//		}),
+//	))
+//
+//	w.ShowAndRun()
+//}
