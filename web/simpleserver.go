91df80ab3ddbee23ded5e4124907cfc1c9d44620
siv <sivsivsree@gmail.com>
2019-04-16 08:59:01 +0400

    middleware use and context in go.

--- web/simpleserver.go
+++ web/simpleserver.go
@@ -2,6 +2,9 @@ package web
 
 import (
 	"encoding/json"
+	"fmt"
+	"github.com/gorilla/context"
+	"github.com/gorilla/mux"
 	"log"
 	"net/http"
 	"time"
@@ -10,6 +13,7 @@ import (
 // Todo is..
 type Todo struct {
 	Name      string
+	Token     string
 	Completed bool
 	Due       time.Time
 }
@@ -19,18 +23,33 @@ type Todos []Todo
 
 func Server() {
 
-	http.HandleFunc("/", rootHandle)
+	r := mux.NewRouter()
+	r.HandleFunc("/test", handler)
+	r.Use(simpleMw)
 
-	log.Fatal(http.ListenAndServe(":9876", nil))
+	log.Fatal(http.ListenAndServe(":9876", r))
 
 }
 
-func rootHandle(w http.ResponseWriter, r *http.Request) {
+func handler(w http.ResponseWriter, r *http.Request) {
+
+	token := fmt.Sprintf("%v", context.Get(r, "token"))
+
 	todos := Todos{
-		Todo{Name: "Write presentation", Due: time.Now()},
+		Todo{Name: "Write presentation", Due: time.Now(), Token: token},
 		Todo{Name: "Host meetup"},
 	}
 
 	json.NewEncoder(w).Encode(todos)
 
+}
+
+func simpleMw(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		// Do stuff here
+		log.Println(r.RemoteAddr, r.RequestURI, r.Method)
+		context.Set(r, "token", "23412342")
+		// Call the next handler, which can be another middleware in the chain, or the final handler.
+		next.ServeHTTP(w, r)
+	})
 }
