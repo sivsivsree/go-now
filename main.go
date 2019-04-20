2ecd1fadd6c06da841b177f141f44f781fda1391
siv <sivsivsree@gmail.com>
2019-04-20 10:05:00 +0400

    🆕🆒💡  Mutex Lock and Wait.Lock Groups..

--- main.go
+++ main.go
@@ -1,10 +1,12 @@
 package main
 
 import (
+	"fmt"
 	"github.com/joho/godotenv"
 	"github.com/sivsivsree/go-now/hello"
 	"github.com/sivsivsree/go-now/web"
 	"log"
+	"sync"
 )
 
 func main() {
@@ -27,22 +29,44 @@ func main() {
 	if err != nil {
 		log.Fatal("Error loading .env file")
 	}
+
+	//for i := 0; i < 1000; i++ {
+	//	 generateMessage("Message String I " + fmt.Sprintf("%v", i))
+	//	 generateMessage("Message String II " + fmt.Sprintf("%v", i))
+	//	 generateMessage("Message String III " + fmt.Sprintf("%v", i))
+	//}
+
 	go web.Server()
-	pings := make(chan string, 1)
-	pongs := make(chan string, 1)
-	ping(pings, "passed message")
-	pong(pings, pongs)
+
 	// fmt.Println(<-pongs)
 
 	web.HandleTCP()
 
 }
 
-func ping(pings chan<- string, msg string) {
-	pings <- msg
+var mutex = &sync.Mutex{}
+var wg sync.WaitGroup
+
+func generateMessage(message string) {
+	//...
+
+	wg.Add(1)
+	go func() {
+		defer wg.Done()
+		printMessage(message)
+	}()
+
+	wg.Wait()
 }
 
-func pong(pings <-chan string, pongs chan<- string) {
-	msg := <-pings
-	pongs <- msg
+func printMessage(resultMessage string) {
+	mutex.Lock()
+	defer mutex.Unlock()
+
+	// Internal Logic : Ignore
+	//if IsTimeEnabled {
+	//	log.Println(resultMessage)
+	//	return
+	//}
+	fmt.Println(resultMessage)
 }
