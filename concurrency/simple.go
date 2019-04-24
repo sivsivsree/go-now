2daa1d03cf8328c2f2bfa48d4195994a1929db88
siv <sivsivsree@gmail.com>
2019-04-24 08:23:16 +0400

    Channels....

---
+++ concurrency/simple.go
@@ -0,0 +1,30 @@
+package main
+
+import (
+	"fmt"
+	"time"
+)
+
+func main() {
+
+	c := make(chan string)
+	go count("Sheep", c)
+
+	for {
+		msg, open := <-c
+
+		if !open {
+			break
+		}
+		fmt.Println(msg)
+	}
+}
+
+func count(thing string, c chan string) {
+	for i := 1; i < 5; i++ {
+		c <- thing
+		time.Sleep(time.Millisecond * 500)
+	}
+
+	close(c)
+}
